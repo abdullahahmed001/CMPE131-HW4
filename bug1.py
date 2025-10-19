fcd86e45d12ade19181b89c519343d8dcbe23ac7
Abdullah Ahmed <121642813+abdullahahmed001@users.noreply.github.com>
2025-10-18 22:52:02 -0700

    Add files via upload

---
+++ bug1.py
@@ -0,0 +1,41 @@
+# TODO: there's code missing in one or more lines below
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+    def draw(self):
+        return ""
+
+class Circle(Base):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size)
+
+    def shape(self):
+        # EXACTLY one newline after "This is a circle"
+        return "This is a circle\n" + self.draw()
+
+    def draw(self):
+        lines = [
+            f"({self.x}, {self.y})",
+            f"{self.size}",
+            "         , - ~ ~ ~ - ,",
+            "     , '               ' ,",
+            "   ,                       ,",
+            "  ,                         ,",
+            " ,                           ,",
+            " ,                           ,",
+            " ,                           ,",
+            "  ,                         ,",
+            "   ,                       ,",
+            "     ,                  , '",
+            "       ' - , _ _ _ ,  '"
+        ]
+        # No trailing newline
+        return "\n".join(lines)
+
+def main():
+    c = Circle(1, 2, 3)
+    print(c.shape())
+main()
